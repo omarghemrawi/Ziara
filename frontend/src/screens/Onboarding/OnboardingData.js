3878d76aa8dea2d9938330cc77b7c05c3de11f76
unknown <ramatobbo5@gmail.com>
2025-07-19 23:57:40 +0300

    Adding the onboarding screens data

---
+++ frontend/src/screens/Onboarding/OnboardingData.js
@@ -0,0 +1,35 @@
+const onboardingScreens = [
+  {
+    id: '1',
+    title: 'Discover Lebanon Like Never Before',
+    description: 'Explore hidden gems, cultural wonders, and local experiences — all in one app.',
+    image: require('../assets/images/map.png'),
+  },
+  {
+    id: '2',
+    title: 'From Ancient Ruins to Sacred Spaces',
+    description: 'Visit Lebanon s rich history and diverse spiritual landmarks.',
+    image: require('../assets/images/window.png'),
+  },
+  {
+    id: '3',
+    title: 'Your Smart Travel Companion',
+    description: 'Ask anything, get help, or discover new places — all through AI chat',
+    image: require('../assets/images/Car.png'),
+  },
+  
+    {
+    id: '4',
+    title: 'Dine Like a Local',
+    description: 'Discover traditional Lebanese restaurants and street food favorites',
+    image: require('../assets/images/Hummus.png'),
+  },
+      {
+    id: '5',
+    title: 'Support Local & Take Home a Memory',
+    description: 'Browse souvenirs and handcrafted treasures from small Lebanese businesses',
+    image: require('../assets/images/tarbushPackage.png'),
+  },
+];
+
+export default onboardingScreens;
