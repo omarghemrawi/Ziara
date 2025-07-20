4ebd56249cabfbe6ffb6d8e06bfc56538560df05
unknown <ramatobbo5@gmail.com>
2025-07-20 21:10:31 +0300

    Creating religious,touristic and restaurants places with dummy data

---
+++ frontend/src/screens/Places/ReligiousPlaces.js
@@ -0,0 +1,36 @@
+
+import React, { useState } from 'react';
+import PlacesSection from '../components/PlaceScreens';
+
+const ReligiousPlaces = () => {
+  const [searchValue, setSearchValue] = useState('');
+  
+  const data = [
+    { image: require('../../assets/images/baytna.jpg') },
+ 
+    // More images
+  ];
+
+  const handleDiscover = () => {
+    console.log('Discover clicked');
+  };
+
+  const handleSave = () => {
+    console.log('Save clicked');
+  };
+
+  return (
+    <PlacesSection
+      title="Religious Places"
+      headerColor="#9a370e"
+      headerImage={require('../../assets/images/baytna.jpg')}
+      data={data}
+      onDiscover={handleDiscover}
+      onSave={handleSave}
+      searchValue={searchValue}
+      onSearchChange={setSearchValue}
+    />
+  );
+};
+
+export default ReligiousPlaces;
