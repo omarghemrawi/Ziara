8b16a34a6d8a10014b68d15f23cbcac16f4e46f2
unknown <ramatobbo5@gmail.com>
2025-07-20 17:58:46 +0300

    .

--- frontend/App.js
+++ frontend/App.js
@@ -2,22 +2,25 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LaunchingScreen from './src/screens/Launching';
+import OnboardingScreen from './src/screens/Onboarding/OnboardingScreen';
 
 
+import MyTabs from './src/screens/naviagtion/Tabs';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-        <Stack.Navigator
-    
-        screenOptions={{
-          headerShown: false, 
-        }}
-      >
-         
-        <Stack.Screen name="Home" component={LaunchingScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+//     <NavigationContainer>
+//         <Stack.Navigator
+//     initialRouteName='Onboarding'
+//         screenOptions={{
+//           headerShown: false, 
+//         }}
+//       >
+//          <Stack.Screen name="Onboarding" options={{headerShown: false}} component={OnboardingScreen} />
+//  <Stack.Screen name="Home" component={MyTabs} options={{headerShown: false}} />
+//         <Stack.Screen name="Launching" component={LaunchingScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
   );
 }
