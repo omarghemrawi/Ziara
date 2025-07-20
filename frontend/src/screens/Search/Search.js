675297ac6279535b2c80097e8f23f7d9e44546c5
unknown <ramatobbo5@gmail.com>
2025-07-20 20:33:08 +0300

    Updating the search page

--- frontend/src/screens/Search/Search.js
+++ frontend/src/screens/Search/Search.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import { View, Text } from 'react-native';
 
-export default function Search() {
+import React, { useState } from 'react';
+import PlacesSection from '../components/PlaceScreens';
+
+const Search = () => {
+  const [searchValue, setSearchValue] = useState('');
+  
+  const data = [
+    { image: require('../../assets/images/baytna.jpg') },
+ 
+    // More images
+  ];
+
+  const handleDiscover = () => {
+    console.log('Discover clicked');
+  };
+
+  const handleSave = () => {
+    console.log('Save clicked');
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Search Page</Text>
-    </View>
+    <PlacesSection
+      title="Search"
+      headerColor="#9a370e"
+      headerImage={require('../../assets/images/baytna.jpg')}
+      data={data}
+      onDiscover={handleDiscover}
+      onSave={handleSave}
+      searchValue={searchValue}
+      onSearchChange={setSearchValue}
+    />
   );
-}
+};
+
+export default Search;
