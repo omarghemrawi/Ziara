19099a7ef295dfb24a5330c12c5f73392b1e2a90
unknown <ramatobbo5@gmail.com>
2025-07-20 13:37:17 +0300

    Adding user icon

--- frontend/src/screens/Home/Home.js
+++ frontend/src/screens/Home/Home.js
@@ -1,13 +1,41 @@
-
 import React from 'react';
-import { View, Text, StyleSheet,Image } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign'; 
 
 export default function Home() {
+  const handleProfilePress = () => {
+    console.log('Profile pressed');
+    // You can navigate to the Profile screen here
+  };
+
   return (
-    <Text>Home</Text>
-  )
+    <View style={styles.container}>
+      {/* Profile Icon Button */}
+      <TouchableOpacity style={styles.profileIcon} onPress={handleProfilePress}>
+        <AntDesign name="user" size={30} color="#000" />
+      
+      </TouchableOpacity>
+
+    
+      <Text style={styles.text}>Find Your Destination</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    paddingTop: 60,
+  },
+  profileIcon: {
+    position: 'absolute',
+    top: 100,
+    right: 40,
+    zIndex: 10,
+  },
+  text: {
+    fontFamily: 'RobotoBold',
+    fontSize: 50,
+    margin: 30,
+  },
 });
