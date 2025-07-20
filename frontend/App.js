a146aafa611692fbf1599579bb10a6ac3ada53e7
unknown <ramatobbo5@gmail.com>
2025-07-20 18:01:02 +0300

    .

--- frontend/App.js
+++ frontend/App.js
@@ -11,16 +11,16 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-{/* //         <Stack.Navigator
-//     initialRouteName='Onboarding'
-//         screenOptions={{
-//           headerShown: false, 
-//         }}
-//       >
-//          <Stack.Screen name="Onboarding" options={{headerShown: false}} component={OnboardingScreen} />
-//  <Stack.Screen name="Home" component={MyTabs} options={{headerShown: false}} />
-//         <Stack.Screen name="Launching" component={LaunchingScreen} />
-//       </Stack.Navigator> */}
+        <Stack.Navigator
+    initialRouteName='Onboarding'
+        screenOptions={{
+          headerShown: false, 
+        }}
+      >
+         <Stack.Screen name="Onboarding" options={{headerShown: false}} component={OnboardingScreen} />
+ <Stack.Screen name="Home" component={MyTabs} options={{headerShown: false}} />
+        <Stack.Screen name="Launching" component={LaunchingScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
