1a2ba93bce4bc636f70ca61963fbac25aae5de05
unknown <ramatobbo5@gmail.com>
2025-07-20 12:04:04 +0300

    Creating the home screen

---
+++ frontend/src/screens/Home/Home.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { View, Text, StyleSheet,Image } from 'react-native';
+
+export default function Home() {
+  return (
+    <Text>Home</Text>
+  )
+}
+
+const styles = StyleSheet.create({
+
+});
