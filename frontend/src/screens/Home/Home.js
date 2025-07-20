f87daa1fdb7493f22080da0920aed2d30c10c881
unknown <ramatobbo5@gmail.com>
2025-07-20 14:30:49 +0300

    Creating three containers

--- frontend/src/screens/Home/Home.js
+++ frontend/src/screens/Home/Home.js
@@ -1,31 +1,47 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native';
 import EvilIcons from 'react-native-vector-icons/EvilIcons'; 
 
 export default function Home() {
   const handleProfilePress = () => {
     console.log('Profile pressed');
-    // You can navigate to the Profile screen here
   };
+ 
 
   return (
     <View style={styles.container}>
       {/* Profile Icon Button */}
-     
       <TouchableOpacity style={styles.profileIcon} onPress={handleProfilePress}>
         <EvilIcons name="user" size={80} color="#000" />
-        <Image style={styles.tarbush} source={require('../../assets/images/miniTarbush.png')}/>
-      
+        <Image style={styles.tarbush} source={require('../../assets/images/miniTarbush.png')} />
       </TouchableOpacity>
 
-    
       <Text style={styles.text}>Find Your Destination</Text>
+
+      {/* Row of Boxes */}
+      <View style={styles.row}>
+        <View style={styles.box}>
+          <Image style={styles.boxImage} source={require('../../assets/images/map.png')} />
+          <Text style={styles.boxText}>nearby</Text>
+        </View>
+
+        <View style={styles.box}>
+          <Image style={styles.boxImage} source={require('../../assets/images/Hummus.png')} />
+          <Text style={styles.boxText}>Popular Foods</Text>
+        </View>
+
+        <View style={styles.box}>
+          <Image style={styles.boxImage} source={require('../../assets/images/map.png')} />
+          <Text style={styles.boxText}>Popular Places</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
+    container: {
     flex: 1,
     paddingTop: 60,
   },
@@ -45,5 +61,29 @@ const styles = StyleSheet.create({
     fontFamily: 'RobotoBold',
     fontSize: 50,
     margin: 30,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  box: {
+    width: 140,
+    height: 140,
+    backgroundColor: '#fac75c',
+    borderRadius: 40,
+    alignItems: 'center',
+    padding: 20,
+     margin:10,
+  },
+  boxImage: {
+    width: 80,
+    height: 60,
+    borderRadius: 60,
+    marginBottom: 10,
+   
+  },
+  boxText: {
+    textAlign: 'center',
+    fontSize: 14,
   },
 });
