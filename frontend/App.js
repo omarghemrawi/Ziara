b6c79415e128c7ed8c63b17fe9153a51ac5b93be
unknown <ramatobbo5@gmail.com>
2025-07-21 14:26:39 +0300

    Save App.js

--- frontend/App.js
+++ frontend/App.js
@@ -3,6 +3,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LaunchingScreen from './src/screens/Launching';
 import OnboardingScreen from './src/screens/Onboarding/OnboardingScreen';
+import Restaurants from './src/screens/Places/Restaurants';
+import ReligiousPlaces from './src/screens/Places/ReligiousPlaces';
+import TouristicPlaces from './src/screens/Places/TouristicPlaces';
 
 
 import MyTabs from './src/screens/naviagtion/Tabs';
@@ -20,6 +23,9 @@ export default function App() {
          <Stack.Screen name="Onboarding" options={{headerShown: false}} component={OnboardingScreen} />
  <Stack.Screen name="Home" component={MyTabs} options={{headerShown: false}} />
         <Stack.Screen name="Launching" component={LaunchingScreen} />
+      <Stack.Screen name="TouristicPlaces" component={TouristicPlaces} />
+<Stack.Screen name="ReligiousPlaces" component={ReligiousPlaces} />
+<Stack.Screen name="Restaurants" component={Restaurants} />
       </Stack.Navigator>
     </NavigationContainer>
   );
