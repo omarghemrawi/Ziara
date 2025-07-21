fc93a75e1115182458f146460d60791e7b52aa06
unknown <ramatobbo5@gmail.com>
2025-07-22 00:18:17 +0300

    Creating the Favorites  screen with dummy image

--- frontend/src/screens/Favorites/Favorites.js
+++ frontend/src/screens/Favorites/Favorites.js
@@ -1,10 +1,86 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ScrollView
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-export default function Favorites() {
+
+export default function Favourites() {
+  const navigation = useNavigation();
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Search Page</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      {/* Header Section */}
+      <View style={styles.header}>
+        <Text style={styles.title}>Favourites</Text>
+        <Image
+          source={require('../../assets/images/Favourites.png')}
+          style={[
+            styles.headerImage,
+         
+          ]}
+        />
+      </View>
+
+      {/* Grid Section */}
+      <View style={styles.grid}>
+        <Image
+          style={styles.imageItem}
+          source={require('../../assets/images/jbeil.jpeg')}
+        />
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  header: {
+    height: 180,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomLeftRadius: 70,
+    borderBottomRightRadius: 70,
+    padding: 20,
+    backgroundColor:'#9a370e',
+   
+  },
+  headerImage: {
+   
+    width: 110,
+    height: 110,
+    position: 'absolute',
+    top: 120,
+    left: 20,
+    
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+    fontFamily: 'RobotoSlabBold',
+    marginTop: 20,
+    marginLeft: 10,
+  },
+
+
+
+  grid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingHorizontal: 20,
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+    imageItem: {
+    width: '27%',
+    height: 90,
+    borderRadius: 15,
+    marginBottom: 10,
+    marginTop:70,
+  },
+});
