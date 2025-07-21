b3257e6262674df88ca1c550184cb9209c0d9de1
unknown <ramatobbo5@gmail.com>
2025-07-22 00:28:33 +0300

    Adjusting the grid and adding place name and place location with clickable dots

--- frontend/src/screens/Favorites/Favorites.js
+++ frontend/src/screens/Favorites/Favorites.js
@@ -4,10 +4,11 @@ import {
   Text,
   StyleSheet,
   Image,
-  ScrollView
+  ScrollView,
+  TouchableOpacity
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-
+import Entypo from 'react-native-vector-icons/Entypo';
 
 export default function Favourites() {
   const navigation = useNavigation();
@@ -19,10 +20,7 @@ export default function Favourites() {
         <Text style={styles.title}>Favourites</Text>
         <Image
           source={require('../../assets/images/Favourites.png')}
-          style={[
-            styles.headerImage,
-         
-          ]}
+          style={styles.headerImage}
         />
       </View>
 
@@ -32,13 +30,41 @@ export default function Favourites() {
           style={styles.imageItem}
           source={require('../../assets/images/jbeil.jpeg')}
         />
+
+        <View style={styles.textContainer}>
+          <Text style={styles.placeName}>Place name</Text>
+          <Text style={styles.placeLocation}>Place location</Text>
+        </View>
+
+        <TouchableOpacity style={styles.iconContainer}>
+          <Entypo name="dots-three-vertical" size={18} color="#000" />
+        </TouchableOpacity>
+        
+      </View>
+       <View style={styles.grid}>
+        <Image
+          style={styles.imageItem}
+          source={require('../../assets/images/jbeil.jpeg')}
+        />
+
+        <View style={styles.textContainer}>
+          <Text style={styles.placeName}>Place name</Text>
+          <Text style={styles.placeLocation}>Place location</Text>
+        </View>
+
+        <TouchableOpacity style={styles.iconContainer}>
+          <Entypo name="dots-three-vertical" size={18} color="#000" />
+        </TouchableOpacity>
+        
       </View>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1 },
+  container: {
+    flex: 1,
+  },
   header: {
     height: 180,
     borderTopLeftRadius: 10,
@@ -46,17 +72,14 @@ const styles = StyleSheet.create({
     borderBottomLeftRadius: 70,
     borderBottomRightRadius: 70,
     padding: 20,
-    backgroundColor:'#9a370e',
-   
+    backgroundColor: '#9a370e',
   },
   headerImage: {
-   
     width: 110,
     height: 110,
     position: 'absolute',
     top: 120,
     left: 20,
-    
   },
   title: {
     color: '#fff',
@@ -66,21 +89,33 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginLeft: 10,
   },
-
-
-
   grid: {
     flexDirection: 'row',
-    flexWrap: 'wrap',
+    alignItems: 'center',
     paddingHorizontal: 20,
-    justifyContent: 'space-between',
-    marginTop: 10,
+    marginTop: 70,
   },
-    imageItem: {
-    width: '27%',
+  imageItem: {
+    width: 90,
     height: 90,
     borderRadius: 15,
-    marginBottom: 10,
-    marginTop:70,
+  },
+  textContainer: {
+    flex: 1,
+    marginLeft: 15,
+    justifyContent: 'center',
+  },
+  placeName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom:10,
+  },
+  placeLocation: {
+    fontSize: 14,
+    color: '#555',
+    marginTop: 2,
+  },
+  iconContainer: {
+    paddingLeft: 10,
   },
 });
