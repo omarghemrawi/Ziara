ae062987742487d64b16d0177753e2f0c806466e
unknown <ramatobbo5@gmail.com>
2025-07-25 19:40:58 +0300

    Creating Place Details Screen

---
+++ frontend/src/screens/Places/PlaceDetailScreen.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ScrollView,
+  TouchableOpacity
+} from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+export default function PlaceDetailScreen() {
+    return(
+        <View></View>
+    )
+}
