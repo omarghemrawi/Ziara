e7d4c3cb8c3c37cb146702f2fdcfdc456ebf6288
unknown <ramatobbo5@gmail.com>
2025-07-28 21:51:17 +0300

    Resolved merge conflicts between backend and main

---
+++ frontend/src/screens/ViewOnMap/location.js
@@ -0,0 +1,19 @@
+
+export function getMapPreview(lat, lng) {
+  // const GOOGLE_API_KEY = 'AIzaSyCEcwxMIkPKG2RoZ8Qa1LKAkB0BHnKU6go';
+  const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${'AIzaSyCEcwxMIkPKG2RoZ8Qa1LKAkB0BHnKU6go'}`;
+  return imagePreviewUrl;
+}
+
+export async function getAddress(lat, lng) {
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${'AIzaSyCEcwxMIkPKG2RoZ8Qa1LKAkB0BHnKU6go'}`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch address!');
+  }
+
+  const data = await response.json();
+  const address = data.results[0].formatted_address;
+  return address;
+}
