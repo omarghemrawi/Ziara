7f33eee4195408f32db51a3693891ea486e09d68
unknown <ramatobbo5@gmail.com>
2025-08-11 21:05:55 +0300

    Updating the logic of the map by sending an url instead of longitude and latitude

--- frontend/src/screens/Map/Map.js
+++ frontend/src/screens/Map/Map.js
@@ -1,32 +1,59 @@
 import React from 'react';
-import MapView, { Marker } from 'react-native-maps';
-import { View, Text } from 'react-native';
+import { View, Text, Button, StyleSheet, Linking } from 'react-native';
 
 export default function MapScreen({ route }) {
-  console.log('Received route params:', route?.params);
-  const { latitude, longitude, title } = route?.params || {};
+  const { locationUrl, title } = route?.params || {};
 
-  if (!latitude || !longitude) {
+  const handleOpenLink = async () => {
+    if (!locationUrl) return;
+
+    const supported = await Linking.canOpenURL(locationUrl);
+    if (supported) {
+      await Linking.openURL(locationUrl);
+    } else {
+      alert('Cannot open this URL: ' + locationUrl);
+    }
+  };
+
+  if (!locationUrl) {
     return (
-      <View>
-        <Text>Invalid location data</Text>
+      <View style={styles.center}>
+        <Text style={styles.text}>No location URL provided</Text>
       </View>
     );
   }
 
   return (
-    <View style={{ flex: 1 }}>
-      <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude,
-          longitude,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
-        }}
-      >
-        <Marker coordinate={{ latitude, longitude }} title={title} />
-      </MapView>
+    <View style={styles.container}>
+      <Text style={styles.title}>{title || 'Go to location'}</Text>
+   
+
+      <Button title=">" onPress={handleOpenLink} color="#FAC75C"/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+
+  text: {
+    fontSize: 18,
+  },
+});
